f76ae14a40ee8d1cb04f6f0fd99c962b04eab025
Yahir Rangel <yahirandres.rangel@gmail.com>
2024-10-19 20:02:14 -0500

    creacion token

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ app = Flask(__name__)
 
 epayco = Epayco({
     'apikey': os.getenv('EPAYCO_PUBLIC_KEY'),
-    'privateKey': os.getenv('EPAYCO_PRIVATE_KEY')
+    'privateKey': os.getenv('EPAYCO_PRIVATE_KEY'),
+    'lenguage': 'ES', #Lenguage pa los messages
+    'test': os.getenv('EPAYCO_TEST') == 'true' #true para modo de PRUEBA, false para modo PRODUCCION
 
 })
+
+
+def create_token(data):
+    try:
+        card_info = {
+            "card[number]": data['card_number'],
+            "card[exp_year]": data['exp_year'],
+            "card[exp_month]": data['exp_month'],
+            "card[cvc]": data['cvc'],
+            "hasCvv": True
+        }
+        token = epayco.token.create(card_info)
+        return token
